168d902905ec67cbb201d9ce30242674e60602be
kactuss <nolan.blanc@grenoble-inp.org>
2025-11-21 09:29:14 +0100

    feat : add doc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"gui"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Ball"},{"l":"boids"},{"l":"conway"},{"l":"core"},{"l":"immigration"},{"l":"schelling"},{"l":"utils"}];updateSearchResults();
